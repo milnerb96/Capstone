781dbb2d6157f6cbbedf19c0b66e8bdcfb1be672
Blaze <blaze_zac@yahoo.com>
2019-02-18 14:28:55 -0600

    Commit that has query values

--- Capstone copy/Main.py
+++ Capstone copy/Main.py
@@ -1,3 +1,5 @@
+#Blaze Milner
+#DNA Sequence Project
 
 from Bio.Seq import Seq #Seq allows for the creation of a sequence object to store the DNA string
 from Bio.Blast import NCBIWWW #Allows access to NCBI wesbite
@@ -24,26 +26,24 @@ def checkNCBI(mySeq):
 def printResults():
 	resultHandle = open("blast.xml")
 	blast_records = NCBIXML.parse(resultHandle) #The returned information from result_handle is a messy XML file. This returns a cleaner version that is easier to read.
-	E_VALUE_THRESH= 0.04
+	E_VALUE_THRESH = 0.0
 
 	for blast_record in blast_records:
 		for alignment in blast_record.alignments:
 			for hsp in alignment.hsps:
-				if hsp.expect < E_VALUE_THRESH:
-					# with open("test.txt", "w") as f:
-					# 	f.write("**** Alignment ****\n")
-					# 	f.write("Sequence: {}\n".format(alignment.title))
-					# 	f.write("Length: {}\n".format(alignment.length))
-					# 	f.write("e value: {}\n".format(hsp.expect))
-					# 	f.write("********************\n ")
-					# 	f.close()
-					print("**** Alignment ****")
+				if hsp.expect is not None:
+					print("\n**** Alignment ****")
 					print("Sequence: {}".format(alignment.title))
 					print("Length: {}".format(alignment.length))
 					print("e value: {}\n".format(hsp.expect))
-					print(hsp.quary[0:75] + "...")
-					print(hsp.match[0:75] + "...")
-					print(hsp.sbjct[0:75] + "...")
+					print(hsp.frame)
+					print(hsp.query_start),
+					print("- {}".format((hsp.query_start + hsp.align_length) - 1))
+					#print(hsp.query[0:75] + "...")
+					#print(hsp.match[0:75] + "...")
+					#print(hsp.sbjct[0:75] + "...")
+				else:
+					print("No matches!")
 
 	resultHandle.close()
 
@@ -55,9 +55,7 @@ def main():
 	for d in dna:
 		print(d)
 		mySeq = dnaSequence(d)
-		#print("Made it to seqeunce")
 		checkNCBI(mySeq)
-		#print("made it to check")
 		printResults()
 		print("%%%%%%%%%%%%%%%%%%Done%%%%%%%%%%%%%%%%%%\n")
 
