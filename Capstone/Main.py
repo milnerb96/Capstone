dda20312bc1ade444572c7c5954b06042c613b36
Blaze <blaze_zac@yahoo.com>
2019-02-20 12:06:48 -0600

    Added the ability to print starting point and ending point of match

--- Capstone/Main.py
+++ Capstone/Main.py
@@ -4,7 +4,7 @@
 from Bio.Seq import Seq #Seq allows for the creation of a sequence object to store the DNA string
 from Bio.Blast import NCBIWWW #Allows access to NCBI wesbite
 from Bio.Blast import NCBIXML #Allows access to BLAST database
-import sys
+import csv
 from textwrap import wrap
 
 def dnaSequence(userSequence):
@@ -19,12 +19,10 @@ def splitSequence(myDNA, dnaSplitter):
 
 def checkNCBI(mySeq):
 	result_handle = NCBIWWW.qblast("blastn", "nt", mySeq)  # Searches the NSCBI BLAST database with the nucleotide sequence # in my_seq
-	with open("blast.xml", "w") as out_handle:
-		out_handle.write(result_handle.read())
-	result_handle.close()
 
-def printResults():
-	resultHandle = open("blast.xml")
+	return result_handle
+
+def printResults(resultHandle):
 	blast_records = NCBIXML.parse(resultHandle) #The returned information from result_handle is a messy XML file. This returns a cleaner version that is easier to read.
 	E_VALUE_THRESH = 0.0
 
@@ -32,20 +30,21 @@ def printResults():
 		for alignment in blast_record.alignments:
 			for hsp in alignment.hsps:
 				if hsp.expect is not None:
-					print("\n**** Alignment ****")
+					print("\n********************** Alignment **********************")
 					print("Sequence: {}".format(alignment.title))
 					print("Length: {}".format(alignment.length))
 					print("e value: {}\n".format(hsp.expect))
-					print(hsp.frame)
-					print(hsp.query_start),
-					print("- {}".format((hsp.query_start + hsp.align_length) - 1))
+					print("Start Value: {}".format(hsp.query_start)),
+					print("End Value: {}".format((hsp.query_start + hsp.align_length) - 1))
 					#print(hsp.query[0:75] + "...")
 					#print(hsp.match[0:75] + "...")
 					#print(hsp.sbjct[0:75] + "...")
+
+					with open("DNA_file.csv", mode = "a") as DNA_file:
+						DNA_writer = csv.writer(DNA_file, delimiter = ",")
+						DNA_writer.writerow([alignment.title,hsp.query_start, (hsp.query_start + hsp.align_length) - 1])
 				else:
 					print("No matches!")
-
-	resultHandle.close()
 
 def main():
 	mySeq = raw_input("Enter Seqeunce: ")
@@ -55,8 +54,8 @@ def main():
 	for d in dna:
 		print(d)
 		mySeq = dnaSequence(d)
-		checkNCBI(mySeq)
-		printResults()
+		blastXML = checkNCBI(mySeq)
+		printResults(blastXML)
 		print("%%%%%%%%%%%%%%%%%%Done%%%%%%%%%%%%%%%%%%\n")
 
 	print("\nCompleted")
